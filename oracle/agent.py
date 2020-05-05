5902e82f57e1da94de832118c8bbd9cbfaf55b07
Vitaly Sopov <v-sopov@yandex-team.ru>
2020-05-05 21:41:24 +0300

    Use single network

--- oracle/agent.py
+++ oracle/agent.py
@@ -10,8 +10,8 @@ from utils import *
 from models import *
 
 
-METRICS_WINDOW = 5  # size of rolling window for metrics
-IMAGES_PERIOD = 15  # show examples of predictions every N episodes
+METRICS_WINDOW = 10  # size of rolling window for metrics
+IMAGES_PERIOD = 10  # show examples of predictions every N episodes
 
 
 class Agent:
@@ -39,9 +39,8 @@ class Agent:
                 r -= 1
         return r
 
-    def train_batch(self, models, optimizers, action, batch):
+    def train_batch(self, model, optimizers, action, batch):
         action = tuple(action)
-        model = models[action]
         optimizer = optimizers[action]
 
         states, next_states = zip(*batch)
@@ -50,7 +49,7 @@ class Agent:
         if torch.cuda.is_available():
             states = states.cuda()
             next_states = next_states.cuda()
-        predictions = model.forward(states)
+        predictions = model.head(states, action)
         loss = F.mse_loss(predictions, next_states)
         optimizer.zero_grad()
         loss.backward()
@@ -71,20 +70,13 @@ class Agent:
         # Setting tensorboadX and variables of interest
         writer = SummaryWriter(log_dir='runs/{}'.format(logfile))
         # Metrics for tensorboard:
-        # losses = np.zeros(METRICS_WINDOW)
-        losses = []
+        losses_by_action = {tuple(action): [] for action in self.possible_actions}
 
-        model_by_action = {
-            tuple(action): OracleNetwork(stack_size=self.stack_size)
-            for action in self.possible_actions
-        }
-        random_model = list(model_by_action.values())[0]
-        # writer.add_graph(random_model)
+        model = OracleNetwork(stack_size=self.stack_size)
         if use_cuda:
             for model in model_by_action.values():
                 model.cuda()
         if load_weights:
-            raise NotImplementedError('Could not yet load model weights')
             ignore_trained_layers = ignore_trained_layers.split(',') if ignore_trained_layers else []
             state_dict = torch.load(load_weights)
             drop_incompatible_layers(model, state_dict, layers_to_ignore=ignore_trained_layers)
@@ -92,13 +84,13 @@ class Agent:
 
         samples_by_action = {tuple(action): [] for action in self.possible_actions}
         optimizer_by_action = {
-            action: optim.Adam(model.parameters(), lr=lr)
-            for (action, model) in model_by_action.items()
+            tuple(action): optim.Adam(model.parameters(), lr=lr)
+            for action in self.possible_actions
         }
         transitions = []
         for episode in range(total_episodes):
             episode_rewards = []
-            episode_losses = []
+            episode_losses_by_action = {tuple(action): [] for action in self.possible_actions}
             game.new_episode()
             variables_cur = {'kills': game.get_game_variable(KILLCOUNT), 'health': game.get_game_variable(HEALTH),
                              'ammo': game.get_game_variable(AMMO2)}
@@ -128,18 +120,20 @@ class Agent:
                     total_reward = np.sum(episode_rewards)
                     print('Episode: {}'.format(episode+1))
                     # Saving metrics
-                    # losses[episode % METRICS_WINDOW] = np.array(episode_losses).mean()
-                    if episode_losses:
-                        losses.append(np.array(episode_losses).mean())
+                    for losses_action, episode_losses in episode_losses_by_action.items():
+                        if episode_losses:
+                            losses_by_action[losses_action].append(np.array(episode_losses).mean())
                     # Update writer
                     if (episode+1) % METRICS_WINDOW == 0:
-                        writer.add_scalar('Train/Loss', np.array(losses).mean(), episode+1)
-                        losses = []
+                        for losses_action in self.possible_actions:
+                            losses_action = tuple(losses_action)
+                            writer.add_scalar('Train/Loss_{}'.format(action_vector_to_name(action)), np.array(losses_by_action[losses_action]).mean(), episode+1)
+                            losses_by_action[losses_action] = []
                     if (episode+1) % IMAGES_PERIOD == 0 and transitions:
                         random_transition = random.choice(transitions)
                         transitions = []
                         writer.add_figure(
-                            'Transitions/{}'.format(random_transition[0]),
+                            'Transitions/{}'.format(action_vector_to_name(random_transition[0])),
                             draw_predictions(random_transition[1]),
                             global_step=episode+1,
                         )
@@ -150,20 +144,19 @@ class Agent:
                     next_stacked_state, stacked_frames = stack_frames(stacked_frames, next_state, False, self.stack_size, self.resize)
                     # If batch is full, learn on it
                     if len(samples_by_action[tuple(action)]) == self.batch_size:
-                        batch_loss, transition = self.train_batch(model_by_action, optimizer_by_action, action, samples_by_action[tuple(action)])
+                        batch_loss, transition = self.train_batch(model, optimizer_by_action, action, samples_by_action[tuple(action)])
                         transitions.append((tuple(action), transition))
-                        episode_losses.append(batch_loss)
+                        episode_losses_by_action[tuple(action)].append(batch_loss)
                         samples_by_action[tuple(action)] = []
                     # Update state variable
                     state = next_stacked_state
 
-            # if (episode+1) % save_freq == 0:
-            #     weights_dir = 'weights/' + logfile
-            #     if not os.path.exists(weights_dir):
-            #         os.makedirs(weights_dir)
-            #     model_file = '{}/{}.pth'.format(weights_dir, episode+1)
-            #     torch.save(dqn_model.state_dict(), model_file)
-            #     print('\nSaved model to ' + model_file)
+            if (episode+1) % save_freq == 0:
+                weights_dir = 'weights/' + logfile
+                if not os.path.exists(weights_dir):
+                    os.makedirs(weights_dir)
+                model_file = '{}/{}.pth'.format(weights_dir, episode+1)
+                torch.save(dqn_model.state_dict(), model_file)
+                print('\nSaved model to ' + model_file)
 
-        writer.export_scalars_to_json("./all_scalars.json")
         writer.close()
--- oracle/models.py
+++ oracle/models.py
@@ -15,14 +15,37 @@ class OracleNetwork(nn.Module):
         super(OracleNetwork, self).__init__()
 
         # Conv Module
-        self.conv_1 = nn.Conv2d(in_channels=stack_size, out_channels=16, kernel_size=7, stride=1, padding=3)
-        self.conv_2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=7, stride=1, padding=3)
-        self.conv_3 = nn.Conv2d(in_channels=32, out_channels=8, kernel_size=7, stride=1, padding=3)
-        self.conv_4 = nn.Conv2d(in_channels=8, out_channels=1, kernel_size=7, stride=1, padding=3)
+        self.conv_1 = nn.Conv2d(in_channels=stack_size, out_channels=16, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+        self.conv_2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+        self.drop2 = nn.Dropout2d(0.2)
+        self.conv_3 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+        self.drop3 = nn.Dropout2d(0.2)
+        self.conv_4 = nn.Conv2d(in_channels=16, out_channels=8, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+        self.drop4 = nn.Dropout2d(0.2)
+        self.conv_left = nn.Conv2d(in_channels=8, out_channels=1, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+        self.conv_shoot = nn.Conv2d(in_channels=8, out_channels=1, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+        self.conv_right = nn.Conv2d(in_channels=8, out_channels=1, kernel_size=7, stride=1, padding=3, padding_mode='reflect')
+
+        self.action_to_layer = {
+            (1, 0, 0): self.conv_left,
+            (0, 1, 0): self.conv_shoot,
+            (0, 0, 1): self.conv_right,
+        }
+
+    def common(self, x):
+        x = F.relu(self.conv_1(x))
+        x = self.drop2(F.relu(self.conv_2(x)))
+        x = self.drop3(F.relu(self.conv_3(x)))
+        x = self.drop4(F.relu(self.conv_4(x)))
+        return x
+    
+    def head(self, x, action):
+        x = self.common(x)
+        layer = self.action_to_layer[action]
+        return F.relu(layer(x))
 
     def forward(self, x):
-        x = F.relu(self.conv_1(x))
-        x = F.relu(self.conv_2(x))
-        x = F.relu(self.conv_3(x))
-        x = F.relu(self.conv_4(x))
-        return x
+        # mainly for visualization
+        x = self.common(x)
+        layer = self.conv_shoot
+        return F.relu(layer(x))
